597e3d29122fff4815579ef2992509c9abd1bbde
nvoleau <n.voleau@groupe-pomona.fr>
2018-04-27 14:03:19 +0200

    modification code

--- js/Person.js
+++ js/Person.js
@@ -3,8 +3,13 @@ function Person (nom,prenom){
 
 	this.prenom = prenom;
 	this.nom=nom;
+	this.age=0;
 }
 
 Person.prototype.getName = function(){
 	return this.prenom+" "+this.nom;
+}
+
+Person.prototype.setAge = function(year){
+    this.age=2018-year;
 }
--- js/main.js
+++ js/main.js
@@ -2,3 +2,5 @@
 //methode es5
 var person = new Person("josephine","baker");
 console.log(person.getName());
+person.setAge(1978);
+console.log(person.age);
